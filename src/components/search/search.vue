855926eb747a311376878b53a861ae60b701eeb9
Rectmoon <1426890675@qq.com>
2018-12-22 21:09:24 +0800

    well done

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-
-  </div>
+  <page :title="('examples.picker')" :desc="('pickerPage.desc')">
+    <div slot="content">
+      <picker-render></picker-render>
+    </div>
+  </page>
 </template>
 
-<script>
-  export default {}
+<script type="text/ecmascript-6">
+import Page from '@/common/page/page.vue'
+import PickerRender from '@/common/page-render/picker-render.vue'
+
+export default {
+  components: {
+    Page,
+    PickerRender
+  }
+}
 </script>
-
-<style scoped>
-
-</style>
